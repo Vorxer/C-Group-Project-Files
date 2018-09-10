5479315ce0142bf7184508f2ac76d26c1b01e771
Vorxer <43013731+Vorxer@users.noreply.github.com>
2018-09-10 16:22:02 +0530

    10/09/2018

--- CPP Part 1/Source.cpp
+++ CPP Part 1/Source.cpp
@@ -19,51 +19,64 @@ struct BankAccount {
 
 int main()
 {
-	std::string pass1("Panzer kampfwagen");
-	std::string temp("Panzer kampfwagen");
 
-	int var = pass1.compare(temp);
-	cout << var;
-	if (var==0)
+	std::string placeholder;
+
+	cout << "Enter your USERID" << endl;
+	std::getline(std::cin, placeholder);
+	cout << "Enter your password" << endl;
+	std::getline(std::cin, placeholder);
+
+	while (true)
+
 	{
-		cout << "ja";
+		int command;
+		cout << "Welcome User" << endl;;
+		cout << "Select Function" << endl;;
+		cout << "1 = Create New Account" << endl;;
+		cout << "2 = Exit" << endl;
+		cin >> command ;
 
+		if (command == 2)
+		{
+			break;
+		}
+
+		if (command == 1){
+			///std::getline(std::cin, name);
+			int userid;
+			int number;
+			double acbalance;
+			std::string acname="Lorem Ipsum Dolor Si Amet";
+			std::string password;
+			cout << "new account creation" << endl;
+			cin.ignore(1000, '\n');
+			cout << "enter your name:";
+			std::getline(std::cin, acname);
+			cout << endl;
+			cout << "enter your password:";
+			std::getline(std::cin, password);
+			cout << endl;
+			cout << "enter your accoutn number :";
+			cin >> number;
+			cout << endl;
+			cout << "enter your accoutnt balance:";
+			cin >> acbalance;
+			cout << endl;
+
+			BankAccount temp1;
+			temp1.acnumber = number;
+			temp1.balance = acbalance;
+			temp1.cname = acname;
+			temp1.password = password;
+
+			cout << "you have entered the fol. information ";
+			cout << "your name " << temp1.cname << endl;
+			cout << "your password " << temp1.password << endl;
+			cout << "your account number " << temp1.acnumber << endl;
+			cout << "your account balance " << temp1.balance << endl;
+		}
 	}
-	
-
-	///std::getline(std::cin, name);
-	int userid;
-	int number;
-	double acbalance;
-	std::string name;
-	std::string password;
-	cout << "new account creation" << endl;
-	cout << "enter your name:";
-	std::getline(std::cin, name);
-	cout << endl;
-	cout << "enter your password:";
-	std::getline(std::cin, password);
-	cout << endl;
-	cout << "enter your accoutn number :";
-	cin >> number;
-	cout << endl;
-	cout << "enter your accoutnt balance:";
-	cin >> acbalance;
-	cout << endl;
-
-	BankAccount temp1;
-	temp1.acnumber=number;
-	temp1.balance=acbalance;
-	temp1.cname=name;
-	temp1.password=password;
-
-	cout << "you have entered the fol. information ";
-	cout << "your name " << temp1.cname << endl;
-	cout << "your password " << temp1.password << endl;
-	cout << "your account number " << temp1.acnumber << endl;
-	cout << "your account balance " << temp1.balance << endl;
-	cin >> number;
-
 	
 
 
--- CPP Part 2/Source.cpp
+++ CPP Part 2/Source.cpp
@@ -41,61 +41,99 @@ int getIndex(BankAccount *input, int target)
 
 int main()
 {
+
 	BankAccount USERID[10];
 
 	///Create 10 accounts for the user
 	///For Test Purposes Only
-	for (int i = 1; i <= 10; i++)
+	while (true)
+
 	{
+
 		int userid;
 		int number;
 		double acbalance;
 		std::string name;
 		std::string password;
-		cout << "new account creation" << endl;
-		cout << "enter your name:";
-		std::getline(std::cin, name);
-		cout << endl;
-		cout << "enter your password:";
-		std::getline(std::cin, password);
-		cout << endl;
-		cout << "enter your accoutn number :";
-		cin >> number;
-		cout << endl;
-		cout << "enter your accoutnt balance:";
-		cin >> acbalance;
-		cout << endl;
 
-		USERID[i] = { number, acbalance, name, password };
+		int origin = 0;
+		int target = 0;
+		int tAmount = 0;
+
+		int command;
+		int i=0;
+
+		cout << "Welcome User" << endl;;
+		cout << "Select Function" << endl;;
+		cout << "1 = Create New Account" << endl;;
+		cout << "2 = Transfer" << endl;
+		cout << "3 = Exit" << endl;
+		cin >> command;
+
+		if (command == 3)
+		{
+			break;
+		}
+
+		if (command == 1)
+		{
+
+			cout << "new account creation" << endl;
+			cin.ignore(1000, '\n');
+			cout << "enter your name:";
+			std::getline(std::cin, name);
+			cout << endl;
+			cout << "enter your password:";
+			std::getline(std::cin, password);
+			cout << endl;
+			cout << "enter your accoutn number :";
+			cin >> number;
+			cout << endl;
+			cout << "enter your accoutnt balance:";
+			cin >> acbalance;
+			cout << endl;
+
+
+			cout << USERID[i].balance << endl;
+			cout << USERID[i].balance << endl;
+
+			USERID[i] = { number, acbalance, name, password };
+			i = i + 1;
+
+		}
+
+		if (command == 2)
+		{
+			///TRANSFER FUNCTION
+
+
+			cout << "Enter the Origin Account";
+			cin >> origin;
+			cout << "Enter the Target Account";
+			cin >> target;
+			cout << "Transfer amount";
+			cin >> tAmount;
+
+			int targetActual = getIndex(USERID, target);
+			int originActual = getIndex(USERID, origin);
+
+			double balanceholder = 0;
+			double newbalance = 0;
+
+			///DEDUCT FROM THE ORIGIN ACCOUNT
+			balanceholder = USERID[originActual].balance;
+			newbalance = balanceholder - tAmount;
+			USERID[originActual].balance = newbalance;
+
+			///ADD TO THE TARGET ACCOUNT
+			balanceholder = USERID[targetActual].balance;
+			newbalance = balanceholder + tAmount;
+			USERID[targetActual].balance = newbalance;
+
+			cout << "Transfer Complete!" << endl;
+			cout << "New Target Account Balance" << USERID[targetActual].balance << endl;
+			cout << "New Origin Account Balance" << USERID[originActual].balance << endl;
+
+		}
 	}
-	///TRANSFER FUNCTION
-
-	int origin = 0;
-	int target = 0;
-	int tAmount = 0;
-	cout << "Enter the Origin Account";
-	cin >> origin;
-	cout << "Enter the Origin Account";
-	cin >> target;
-	cout << "Transfer amount";
-	cin >> tAmount;
-
-	int targetActual = getIndex(USERID, target);
-	int originActual = getIndex(USERID, origin);
-
-	double balanceholder = 0;
-	double newbalance = 0;
-
-	///DEDUCT FROM THE ORIGIN ACCOUNT
-	balanceholder = USERID[originActual].balance;
-	newbalance = balanceholder - tAmount;
-	USERID[originActual].balance = newbalance;
-
-	///ADD TO THE TARGET ACCOUNT
-	balanceholder = USERID[targetActual].balance;
-	newbalance = balanceholder + tAmount;
-	USERID[targetActual].balance = newbalance;
-
-
-
 }
